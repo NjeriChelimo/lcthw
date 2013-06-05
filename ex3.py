34c893567cee651d84426ab1563a951329dcd60d
NjeriChelimo <martha.c.chumo@gmail.com>
2013-06-05 22:11:54 +0300

    added first practice files

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:" # prints out count of chicken.
+
+print "Hens", 25.0 + 30.0 / 6.0 # print no. of hens
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 # print no. of rooster
+
+print "Now I will count the eggs:" #print out "now I will count eggs"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # print number of egges
+
+print "Is it two that  3 + 2 < 5 - 7?"  # print a que
+
+print 3.0 + 2.0 < 5.0 - 7.0 #print something
+
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it is False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 # got it now.
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
---
+++ ex6.py
@@ -0,0 +1,41 @@
+# prints out the kinds of prople there are.
+x = "There are %d types of people." % 10
+
+# assigning the variable binary
+binary = "binary"
+
+# assigning the variable do_not
+do_not = "don't"
+
+# assigning the variable y
+y = "Those who know %s and those who  %s." % (binary, do_not)
+
+# printing out the value of x
+print x
+
+# printing out the value of y
+print y
+
+# printing out a sentense 1
+print "I said: %r." % x
+
+# printing out sentense 2
+print "I also said: '%s'." % y
+
+# assigning the variable hilarious
+hilarious = False
+
+# assigning the variable joke_evaluation
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+# printing out the joke evaluation
+print joke_evaluation % hilarious
+
+# declaring w
+w = "This is the left side of..."
+
+# declaring e
+e = "a string with a right side."
+
+# printing the dumb sentense.
+print w + e
